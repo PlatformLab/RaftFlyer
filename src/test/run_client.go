1683a4c34d1efa5c37652751d83947d9fb5f38a5
Emma Dauterman <emmadauterman@DN51q80r.SUNet>
2018-04-07 12:54:42 -0700

    Added very basic client test, passes

---
+++ src/test/run_client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "test/keyValStore"
+    "raft"
+    "fmt"
+    "strings"
+    "time"
+)
+
+func main() {
+    trans, err := raft.NewTCPTransport("127.0.0.1:5000", nil, 2, time.Second, nil)
+    if err != nil {
+        fmt.Println("Error with creating TCP transport: ", err)
+        return
+    }
+    servers := []raft.ServerAddress{"127.0.0.1:8000","127.0.0.1:8001","127.0.0.1:8002"}
+    c := keyValStore.CreateClient(trans, servers)
+    c.Set("foo","bar")
+    str, getErr := c.Get("foo")
+    if getErr != nil || strings.Compare(str,"bar") != 0 {
+        fmt.Println("Failure: error was ", getErr, " and returned string was ", str)
+    } else {
+        fmt.Println("Success!")
+    }
+}
