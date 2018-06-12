c94ea90214cca01936963348e805d82301560943
Emma Van Amburgh Dauterman <evd@rcmaster.scs.stanford.edu>
2018-06-12 16:24:42 -0700

    produce preliminary cdf

--- src/test/bench/generateFigures.py
+++ src/test/bench/generateFigures.py
@@ -1,5 +1,8 @@
 from runExperiment import runExper
-#import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
 
 clients = ["192.168.1.123:5000"]
 
@@ -10,3 +13,13 @@ print "LATENCIES: "
 print latencies
 print "THROUGHPUT: "
 print throughput
+
+latencies = np.array(latencies, dtype=np.int32)
+#latencies = latencies.astype(np.int)
+num_bins = 20
+counts, bin_edges = np.histogram(latencies, bins=num_bins, normed=True)
+cdf = np.cumsum(counts)
+plt.plot(bin_edges[1:], cdf)
+#plt.hist(latencies, normed=True, cumulative=True, label='CDF', histtype='step', alpha=0.8, color='k')
+plt.title("CURP CDF")
+plt.savefig("cdf.png")
