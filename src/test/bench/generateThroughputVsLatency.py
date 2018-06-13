47dca81fb872d2a5fd0e704f92bbf141f0778b21
Emma Van Amburgh Dauterman <evd@rcmaster.scs.stanford.edu>
2018-06-13 11:19:34 -0700

    script fixes

--- src/test/bench/generateThroughputVsLatency.py
+++ src/test/bench/generateThroughputVsLatency.py
@@ -7,8 +7,9 @@ import sys
 
 # Firist command line argument is % commutative requests
 commPercent = sys.argv[1]
-averageRuns=5
-maxThreads=10
+averageRuns=1#5
+maxThreads=2
+# Add more clients with ports, iterate over by adding clients each time
 clients = ["192.168.1.123:5000", "192.168.1.124:5000", "192.168.1.125:5000"]
 throughputList = []
 latencyList = []
@@ -16,9 +17,7 @@ for i in range(1, maxThreads):
     avgThroughput = 0
     avgLatency = 0
     for j in range(0, averageRuns):
-        latencies, throughput =
-        runExper("/home/evd/RaftFlyer/src/test/bench/config", clients, i, 100,
-                True, int(commPercent))
+        latencies, throughput = runExper("/home/evd/RaftFlyer/src/test/bench/config", clients, i, 100, True, int(commPercent))
         latencies = np.array(latencies, dtype=np.int32)
         avgLatency += np.mean(latencies) 
         avgThroughput += throughput
