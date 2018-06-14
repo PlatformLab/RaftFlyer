191ff6dccb9faa563c0a580e9b6677626550186d
Emma Van Amburgh Dauterman <evd@rcmaster.scs.stanford.edu>
2018-06-13 21:55:58 -0700

    Add testing documentation and scripts to generate throughput vs latency for different % commutative reqs

---
+++ src/test/bench/TESTING.md
@@ -0,0 +1,8 @@
+Run scripts from rcmaster. 
+
+Run generateFigures.sh to create CDFs of latencies with 100%, 95%, and 90%
+commutative requests and throughput vs latency for 100%, 95%, and 90% comutative
+requests. Graphs created in current working directory.
+
+Uses rc20-25 (to change this, change IP addresses in generateCdf.py and
+generateThroughputVsLatency.py).
--- src/test/bench/generateThroughputVsLatency.py
+++ src/test/bench/generateThroughputVsLatency.py
@@ -7,7 +7,6 @@ import sys
 
 # Firist command line argument is % commutative requests
 commPercent = sys.argv[1]
-averageRuns=1#5
 maxThreads = 10
 # Add more clients with ports, iterate over by adding clients each time
 clients = ["192.168.1.123", "192.168.1.124", "192.168.1.125"]
@@ -20,16 +19,12 @@ for i in range(1, maxThreads):
             tempClients.append(client + ":" + str(5000 + j))
     avgThroughput = 0
     avgLatency = 0
-    for j in range(0, averageRuns):
-        latencies, throughput = runExper("/home/evd/RaftFlyer/src/test/bench/config", tempClients, 1, 100, True, int(commPercent))
-        latencies = np.array(latencies, dtype=np.int32)
-        avgLatency += np.mean(latencies) 
-        avgThroughput += throughput
-    avgThroughput /= float(averageRuns)
-    avgLatency /= float(averageRuns)
-    throughputList.append(avgThroughput)
-    latencyList.append(avgLatency)
+    latencies, throughput = runExper("/home/evd/RaftFlyer/src/test/bench/config", tempClients, 1, 100, True, int(commPercent))
+    latencies = np.array(latencies, dtype=np.int32)
+    throughputList.append(throughput)
+    latencyList.append(np.mean(latencies))
 plt.plot(throughputList, latencyList, linestyle='-', marker='o')
 plt.xlabel("Throughput (ops/sec)")
 plt.ylabel("Latency (microsec)")
-plt.savefig("ThroughputVsLatency.png")
+plt.title("Throughput vs Latency with %s%% commutative requests" % commPercent)
+plt.savefig("ThroughputVsLatency-%s.png" % commPercent)
